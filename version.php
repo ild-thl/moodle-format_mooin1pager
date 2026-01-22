4ea32bdb9310fe744b2ca58961689e733547479b
muschthom <14013721+muschthom@users.noreply.github.com>
2026-01-22 10:39:28 +0100

    Update version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025071803;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026012200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100100;        // Requires this Moodle version.
 $plugin->component = 'format_mooin1pager';    // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
